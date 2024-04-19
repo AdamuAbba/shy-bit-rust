6f5f6a3ef4a28d0001a1574e0777f2eab535f5f5
AdamuAbba <abbaadamu302@gmail.com>
2024-04-19 23:28:08 +0100

    add minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use clap::{command, Arg, ArgMatches};
 
 fn main() {
-    let match_results: ArgMatches = command!()
-        .arg(Arg::new("wallet"))
-        .arg(Arg::new("address"))
+    let match_results: ArgMatches = command!().about("This a simply a rust app to help us understand HD wallets by making use of a JSON RPC client to make calls to a bitcoin core backend.")
+        .arg(
+            Arg::new("wallet")
+                .long("wallet")
+                .help("wallet specific commands")
+                .aliases(["Wallet", "wlet"]),
+        )
+        .arg(
+            Arg::new("address")
+                .long("address")
+                .help("address specific commands")
+                .aliases(["add", "Address"]),
+        )
         .get_matches();
 }
